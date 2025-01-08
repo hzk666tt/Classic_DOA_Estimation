42442e134c79dfcf9b76ea6d03673c25376333a6
两仪式 <50283218+hzk666tt@users.noreply.github.com>
2025-01-08 16:51:52 +0800

    Update CBF_DOA.m

--- DOA_CBF/CBF_DOA.m
+++ DOA_CBF/CBF_DOA.m
@@ -1,4 +1,4 @@
-function CBF_output = CBF_DOA(X, target_num, d_lambda, Phi_set)
+function [CBF_output_wave, CBF_output_degree] = CBF_DOA(X, target_num, d_lambda, phi_step, Phi_set)
     % X: 输入信号
     % target_num: 信源数
     % d_lambda: 阵元间距波长比
@@ -7,6 +7,7 @@ function CBF_output = CBF_DOA(X, target_num, d_lambda, Phi_set)
 
     row = size(X, 1); % 阵列排列
     CBF = zeros(1, length(Phi_set)); %初始化
+    max_loc = zeros(1, target_num);
 
     for i = 1:length(Phi_set)
         Weight = exp(-1j * 2 * pi * (0:row -1)' * d_lambda * sind(Phi_set(i)));
@@ -15,5 +16,22 @@ function CBF_output = CBF_DOA(X, target_num, d_lambda, Phi_set)
 
     %log_CBF = log10(CBF / max(CBF));
     %CBF_output = (log_CBF - min(log_CBF)) / max(log_CBF - min(log_CBF));
-    CBF_output = CBF / max(CBF);
+    CBF_normal = CBF / max(CBF);
+
+    [pks, locs] = findpeaks(CBF_normal); %寻找全部谱峰及索引
+    %[pks, id] = sort(pks);
+    %locs = locs(id(end - target_num + 1:end)) - 1;
+    %CBF_output_degree = locs * phi_step - 90;
+
+    for j = 1:target_num
+        [~, id] = max(pks);
+        max_loc(:, j) = locs(id) -1;
+        pks(id) = -inf;
+    end %寻找前target_num个最大值及其索引并保存
+
+    CBF_output_degree = max_loc * phi_step - 90;
+
+    CBF_output_wave = log10(CBF_normal);
+    CBF_output_degree = sort(CBF_output_degree);
+    disp('CBF估计结果'); disp(CBF_output_degree);
 end
