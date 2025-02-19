5042713bf3dfdb96064dbbef3007a8b301e7b9da
两仪式 <50283218+hzk666tt@users.noreply.github.com>
2025-02-19 23:23:48 +0800

    Update MVDR_DOA.m

--- DOA_MVDR/MVDR_DOA.m
+++ DOA_MVDR/MVDR_DOA.m
@@ -1,4 +1,4 @@
-function [MVDR_output_wave, MVDR_output_degree] = MVDR_DOA(X, target_num, d_lambda, phi_step, Phi_set)
+function [MVDR_output_wave, MVDR_output_degree] = MVDR_DOA(X_N, MIMO_Ant_num, snapshot, target_num, phi_step, phi_set)
     % X:                    输入信号
     % target_num:           信源数
     % d_lambda:             阵元间距波长比
@@ -8,15 +8,12 @@ function [MVDR_output_wave, MVDR_output_degree] = MVDR_DOA(X, target_num, d_lamb
 
     tic %计时开始
 
-    row = size(X, 1);
-    column = size(X, 2);
-    R = X * X' / column;
-    %R = X * X';
-    MVDR = zeros(1, length(Phi_set)); %初始化
+    R = X_N * X_N' / snapshot;
+    MVDR = zeros(1, length(phi_set)); %初始化
     max_loc = zeros(1, target_num);
 
-    for i = 1:length(Phi_set)
-        A_vector = exp(-1j * 2 * pi * (0:row -1)' * d_lambda * sind(Phi_set(i)));
+    for i = 1:length(phi_set)
+        A_vector = exp(-1j * pi * (0:MIMO_Ant_num -1)' * sind(phi_set(i)));
         MVDR(i) = 1 / abs(A_vector' * R ^ (-1) * A_vector);
     end
 
@@ -33,11 +30,11 @@ function [MVDR_output_wave, MVDR_output_degree] = MVDR_DOA(X, target_num, d_lamb
         pks(id) = -inf;
     end %寻找前target_num个最大值及其索引并保存
 
-    MVDR_output_degree = max_loc * phi_step - (length(Phi_set) - 1) * phi_step / 2;
+    MVDR_output_degree = max_loc * phi_step - (length(phi_set) - 1) * phi_step / 2;
 
     toc %计时结束
 
-    MVDR_output_wave = log10(MVDR_normal);
+    MVDR_output_wave = 10 * log10(MVDR_normal);
     MVDR_output_degree = sort(MVDR_output_degree);
     disp(['MVDR估计结果：', newline, num2str(MVDR_output_degree), newline]);
     %disp(MVDR_output_degree);
