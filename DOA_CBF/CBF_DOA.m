bca1ce8dfe7b2d38236bfdd42d721abe6d87a2f8
两仪式 <50283218+hzk666tt@users.noreply.github.com>
2025-02-19 23:22:42 +0800

    Update CBF_DOA.m

--- DOA_CBF/CBF_DOA.m
+++ DOA_CBF/CBF_DOA.m
@@ -1,20 +1,18 @@
-function [CBF_output_wave, CBF_output_degree] = CBF_DOA(X, target_num, d_lambda, phi_step, Phi_set)
-    % X: 输入信号
+function [CBF_output_wave, CBF_output_degree] = CBF_DOA(X_N, MIMO_Ant_num, target_num, phi_step, phi_set)
+    % X_N: 输入信号
     % target_num: 信源数
-    % d_lambda: 阵元间距波长比
     % Phi_set: 角度区间
     % CBF_output_wave: DOA估计谱
     % CBF_output_degree: DOA估计角度
 
     tic %计时开始
 
-    row = size(X, 1); % 阵列排列
-    CBF = zeros(1, length(Phi_set)); %初始化
+    CBF = zeros(1, length(phi_set)); %初始化
     max_loc = zeros(1, target_num);
 
-    for i = 1:length(Phi_set)
-        Weight = exp(-1j * 2 * pi * (0:row -1)' * d_lambda * sind(Phi_set(i)));
-        CBF(i) = abs(Weight' * (X * X') * Weight);
+    for i = 1:length(phi_set)
+        Weight = exp(-1j * pi * (0:MIMO_Ant_num -1)' * sind(phi_set(i)));
+        CBF(i) = abs(Weight' * (X_N * X_N') * Weight);
     end
 
     CBF_normal = CBF / max(CBF);
@@ -30,11 +28,11 @@ function [CBF_output_wave, CBF_output_degree] = CBF_DOA(X, target_num, d_lambda,
         pks(id) = -inf;
     end %寻找前target_num个最大值及其索引并保存
 
-    CBF_output_degree = max_loc * phi_step - (length(Phi_set) - 1) * phi_step / 2;
+    CBF_output_degree = max_loc * phi_step - (length(phi_set) - 1) * phi_step / 2;
 
     toc %计时结束
 
-    CBF_output_wave = log10(CBF_normal);
+    CBF_output_wave = 10 * log10(CBF_normal);
     CBF_output_degree = sort(CBF_output_degree);
     disp(['CBF估计结果：', newline, num2str(CBF_output_degree), newline]);
     %disp(CBF_output_degree);
