2d0d258d4922af58336e31dc2160273647147598
两仪式 <50283218+hzk666tt@users.noreply.github.com>
2025-02-19 23:23:20 +0800

    Update MUSIC_DOA.m

--- DOA_MUSIC/MUSIC_DOA.m
+++ DOA_MUSIC/MUSIC_DOA.m
@@ -1,23 +1,20 @@
-function [MUSIC_output_wave, MUSIC_output_degree] = MUSIC_DOA(X, target_num, d_lambda, phi_step, Phi_set)
+function [MUSIC_output_wave, MUSIC_output_degree] = MUSIC_DOA(X_N, MIMO_Ant_num, snapshot, target_num, phi_step, Phi_set)
     % X: 输入信号
     % target_num: 信源数
-    % d_lambda: 阵元间距波长比
     % Phi_set: 角度区间
     % MUSIC_output_wave: DOA估计谱
     % MUSIC_output_degree: 估计角度
 
     tic %计时开始
 
-    row = size(X, 1); %阵元数
-    column = size(X, 2); %快拍数
-    R = X * X' / column; %计算协方差矩阵
+    R = X_N * X_N' / snapshot; %计算协方差矩阵
     [R_Vector, ~] = eig(R); %计算特征向量并保存
-    Un = R_Vector(:, 1:row - target_num);
+    Un = R_Vector(:, 1:MIMO_Ant_num - target_num);
     MUSIC = zeros(1, length(Phi_set)); %初始化
     max_loc = zeros(1, target_num);
 
     for i = 1:length(Phi_set)
-        A_vector = exp(-1j * 2 * pi * (0:row -1)' * d_lambda * sind(Phi_set(i)));
+        A_vector = exp(-1j * pi * (0:MIMO_Ant_num -1)' * sind(Phi_set(i)));
         MUSIC(i) = 1 / abs((A_vector' * Un * Un' * A_vector));
     end
 
@@ -38,7 +35,7 @@ function [MUSIC_output_wave, MUSIC_output_degree] = MUSIC_DOA(X, target_num, d_l
 
     toc %计时结束
 
-    MUSIC_output_wave = log10(MUSIC_normal);
+    MUSIC_output_wave = 10 * log10(MUSIC_normal);
     MUSIC_output_degree = sort(MUSIC_output_degree);
     disp(['MUSIC估计结果：', newline, num2str(MUSIC_output_degree), newline]);
     %disp(MUSIC_output_degree);
