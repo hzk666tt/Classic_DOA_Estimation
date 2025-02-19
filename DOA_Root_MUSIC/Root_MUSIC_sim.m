026086152f55c95c28da95825af0b5c7655b50da
两仪式 <50283218+hzk666tt@users.noreply.github.com>
2025-02-19 23:27:01 +0800

    Update Root_MUSIC_sim.m

--- DOA_Root_MUSIC/Root_MUSIC_sim.m
+++ DOA_Root_MUSIC/Root_MUSIC_sim.m
@@ -1,34 +1,37 @@
 close all; clear all; clc;
-% ADC Parameters
-snr = 5; % input SNR (dB)
+
+snr = 5; % 信噪比
 snapshot = 128; % 快拍数
 
-d_lambda = 0.5; % 阵元间距波长比
 target_theta = [-1.1 9.9]; % 目标角度
 target_theta_rad = deg2rad(target_theta); % 转为弧度
 target_num = length(target_theta_rad); % 目标角度数
-RX_num = 8; % 阵元数
-RX_set = 0:1:RX_num - 1; % 阵列排列
+
+Tx_num = 2;
+Rx_num = 4;
+
+MIMO_Ant_num = Tx_num * Rx_num; % MIMO阵元数
+MIMO_Ant_set = 0:1:MIMO_Ant_num - 1; % MIMO阵列排列
 
 phi_start = -90; % 定义角区间起点
 phi_end = 90; % 定义角区间终点
-phi_step = 0.1;
-Phi_set = phi_start:phi_step:phi_end; % 定义叫区间
+phi_step = 0.1; %定义网格步长
+phi_set = phi_start:phi_step:phi_end; % 定义角区间
 
 % generate signal
-Signal = randn(target_num, snapshot) + 1j * randn(target_num, snapshot); % 产生target_num*snapshot的随机信号
-A = exp(-1j * 2 * pi * RX_set' * d_lambda * sin(target_theta_rad)); % 导向矩阵
-X = A * Signal;
-X = awgn(X, snr, 'measured'); % 加白噪声
+S = randn(target_num, snapshot) + 1j * randn(target_num, snapshot); % 产生target_num*snapshot的随机信号
+A = exp(-1j * pi * MIMO_Ant_set' * sin(target_theta_rad)); % 导向矩阵
+X = A * S;
+X_N = awgn(X, snr, 'measured'); % 加白噪声
 
 % DOA estimation
-result_Root_MUSIC_degree = Root_MUSIC_DOA(X, target_num);
+result_Root_MUSIC_degree = Root_MUSIC_DOA(X_N, MIMO_Ant_num, snapshot, target_num);
 plot(result_Root_MUSIC_degree, 0, 'o', 'Color', 'b'); hold on;
 xline(target_theta(1), '--', 'Color', 'k'); hold on;
 xline(target_theta(2), '--', 'Color', 'k'); hold on;
 xlabel('\theta(\circ)'); ylabel('Normalized Spatial Spectrum(dB)');
 legend('Root-MUSIC', '', ...
     ['Real \theta (', num2str(target_theta(1)), '\circ, ', ...
-    num2str(target_theta(2)), '\circ)'], '', 'Color', 'none'); % 图例
+     num2str(target_theta(2)), '\circ)'], '', 'Color', 'none'); % 图例
 set(gca, 'color', 'none', 'FontName', 'Times New Roman', 'LooseInset', get(gca, 'TightInset'));
 ax = gca; copygraphics(ax, 'ContentType', 'vector', 'BackgroundColor', 'none');
